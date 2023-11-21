17d2369b209b1bf44c652cb3b97ecbadb7435c26
bacmarcio <bac.marcio@gmail.com>
2023-11-21 18:53:47 -0300

    Layout

---
+++ releases.php
@@ -0,0 +1,5 @@
+<?php 
+
+print_r($_GET);
+
+echo "<h1>OK</h1>";
--- rotas.php
+++ rotas.php
@@ -5,34 +5,21 @@ $url = isset($_GET['url']) ? $_GET['url'] : '';
 
 // Defina rotas para URLs específicas
 $routes = [
-    'noticias'         => 'noticias.php',
-    'add-noticia'      => 'add-noticia.php',
-    'editar-noticia'   => 'editar-noticia.php',
-    'login'            => 'login.php',
-    'logout'           => 'login.php',
-    'cadastro'         => 'cadastro.php',
-    'editar-perfil'    => 'editar-perfil.php',
+    'quem-somos' => 'quem-somos.php',
+    'releases' => 'releases.php',
 ];
 
 // Verifique se a URL corresponde a uma rota válida
-if (array_key_exists($url, $routes)) 
-{
+if (array_key_exists($url, $routes)) {
     $file = $routes[$url];
-} 
-elseif (preg_match('/^editar-noticia\/([a-zA-Z-0-9-_]+)$/', $url, $matches)) 
-{
-    // Tratar URLs do tipo "editar-noticia/algum-id"
-    $id = $matches[1];
-    $file = 'editar-noticia.php';
-} 
-elseif (preg_match('/^editar-perfil\/([a-zA-Z-0-9-_]+)$/', $url, $matches)) 
-{
-    // Tratar URLs do tipo "editar-noticia/algum-id"
-    $id = $matches[1];
-    $file = 'editar-perfil.php';
-} 
-else 
-{
+} elseif (preg_match('/^releases\/([a-zA-Z-0-9-_]+)$/', $url, $matches)) {
+    // Tratar URLs do tipo "releases/titulo-da-release"
+    $tituloRelease = $matches[1];
+    $file = 'releases.php';
+
+    // Passe o título da release para a página
+    $_GET['titulo_release'] = $tituloRelease;
+} else {
     // Lógica padrão para URLs não reconhecidas
     $file = 'index.php';
 }
